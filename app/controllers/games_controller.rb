86158b73435068ad4bd914c6a96fc96135d1fe00
Hernan Romero <hernrome@gmail.com>
2020-01-16 21:18:19 +0000

    Added comparison of input with gird

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,24 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
-    @letters = ('a'..'z').to_a.sample(8)
+    @letters = ('A'..'Z').to_a.sample(9)
   end
 
   def score
+    @input = params[:guess].upcase
+    @outputLetters = params[:grid]
+    @score = @input.chars.all? do |letter|
+    binding.pry
+      @input.count(letter) <= @outputLetters.count(letter)
+    end
   end
+
+  # def english_word?(word)
+  #   response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+  #   json = JSON.parse(response.read)
+  #   return json['found']
+  # end
+
 end
